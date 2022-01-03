70fdf9954088542bf6cd9f76fdcc29dcdcc2da64
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-02 22:53:03 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Heath Fashina on 2022-01-02.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
