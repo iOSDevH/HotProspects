f31c1ba9c90e69217b55e0df5bbd27bff7442bc1
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-04 20:00:00 -0500

    Day82

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Heath Fashina on 2022-01-03.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        people = []
+    }
+}
